164c23232ea08fe6bc5c35bcaab0749175e93626
chaoism <longfellow@wangxh.local>
2022-06-28 11:44:38 +0800

    update MergeTwoLists

--- LinkedList/include/MergeTwoLists.h
+++ LinkedList/include/MergeTwoLists.h
@@ -3,7 +3,27 @@
 class Solution
 {
 public:
-    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
     {
+        ListNode *res = new ListNode(-1);
+
+        ListNode *prev = res;
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                prev->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        prev->next = l1 == nullptr ? l2 : l1;
+
+        return res->next;
     }
 };
